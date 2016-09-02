450c25c55079bebb23ca8ef0358aa04784aec935
axocomm <axocomm@gmail.com>
2016-09-02 19:30:14 -0400

    Add routes for IPv4 address allocation and unallocation

--- inc/IPv4Controller.class.php
+++ inc/IPv4Controller.class.php
@@ -12,10 +12,82 @@ class IPv4Controller {
 
   public static function allocateIP($request, $response, $args) {
     $ip = $args['ip'];
-    $object_id = $args['object'];
+    $identifier = $request->getQueryParam('object');
+    $name = $request->getQueryParam('name', '');
+    $type = $request->getQueryParam('type', 'regular');
+
+    if (!$identifier) {
+      return $response->withJson(array(
+        'success' => false,
+        'error'   => 'Missing object'
+      ));
+    }
+
+    $object = ObjectUtils::getObject(array(
+      'any' => array(
+        'name' => $identifier,
+        'id'   => $identifier,
+        'FQDN' => $identifier
+      )
+    ));
+
+    if ($object === null) {
+      return $response->withJson(array(
+        'success' => false,
+        'error'   => "Object {$identifier} does not exist"
+      ));
+    }
+
+    $object_id = $object['id'];
+    $ip_bin = IPv4Utils::ipToBin($ip);
+
+    bindIPv4ToObject(
+      $ip_bin,
+      $object_id,
+      $name,
+      $type
+    );
+
     $response->withJson(array(
       'success' => true,
-      'result'  => IPv4Utils::ipToInt($ip)
+      'message' => "Bound {$ip} to {$identifier}"
+    ));
+  }
+
+  public static function unallocateIP($request, $response, $args) {
+    $ip = $args['ip'];
+    $identifier = $request->getQueryParam('object');
+
+    if (!$identifier) {
+      return $response->withJson(array(
+        'success' => false,
+        'error'   => 'Missing object'
+      ));
+    }
+
+    $object = ObjectUtils::getObject(array(
+      'any' => array(
+        'name' => $identifier,
+        'id'   => $identifier,
+        'FQDN' => $identifier
+      )
+    ));
+
+    if ($object === null) {
+      return $response->withJson(array(
+        'success' => false,
+        'error'   => "Object {$identifier} does not exist"
+      ));
+    }
+
+    $object_id = $object['id'];
+    $ip_bin = IPv4Utils::ipToBin($ip);
+
+    unbindIPFromObject($ip_bin, $object_id);
+
+    $response->withJson(array(
+      'success' => true,
+      'message' => "Unbound {$ip} from {$identifier}"
     ));
   }
 }
