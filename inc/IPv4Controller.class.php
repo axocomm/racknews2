c6d8cc2176ae72a2097576f02af892b995bfb0f6
axocomm <axocomm@gmail.com>
2016-09-02 17:49:45 -0400

    Use getIPv4Allocations RackTables function for /ipv4

--- inc/IPv4Controller.class.php
+++ inc/IPv4Controller.class.php
@@ -3,8 +3,10 @@ namespace Racknews;
 
 class IPv4Controller {
   public static function getIPv4Allocations($request, $response, $args) {
-    $addrs = IPv4Utils::getAddresses(true);
-    $addrs = array_walk_recursive($addrs, 'utf8_encode');
-    $response->withJson($addrs);
+    $allocations = IPv4Utils::getAddresses();
+    $response->withJson(array(
+      'success'     => true,
+      'allocations' => $allocations
+    ));
   }
 }
