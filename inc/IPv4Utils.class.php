3eedc3c90d9567c0f88ae234170542bd856e5d01
axocomm <axocomm@gmail.com>
2016-09-02 18:36:39 -0400

    WIP IP allocation

--- inc/IPv4Utils.class.php
+++ inc/IPv4Utils.class.php
@@ -9,4 +9,15 @@ class IPv4Utils {
   public static function getNetworks() {
     return scanRealmByText('ipv4net');
   }
+
+  public static function ipToInt($ip_str) {
+    $octets = array_map(function ($oc) {
+      return (int) $oc;
+    }, explode('.', $ip_str));
+
+    return ($octets[0] * pow(256, 3)) +
+           ($octets[1] * pow(256, 2)) +
+           ($octets[2] * pow(256, 1)) +
+           ($octets[3] * pow(256, 0));
+  }
 }
