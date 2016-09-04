4de8a90a281eb03bfd378028b5d563573bc80145
axocomm <axocomm@gmail.com>
2016-09-04 00:17:50 -0400

    Move top-level get_objects to Object::all

--- app/models/Object.class.php
+++ app/models/Object.class.php
@@ -7,7 +7,31 @@ class Object {
   const MATCH_ALL = 'all';
 
   public static function all() {
-    return get_objects();
+    $rt_objects = scanRealmByText('object');
+
+    return array_map(
+      function ($object) {
+        $info = spotEntity('object', $object['id']);
+        amplifyCell($info);
+
+        $attrs = array_reduce(
+          getAttrValues($object['id']),
+          function ($attr_acc, $record) {
+            if (!isset($record['name'])) {
+              throw new \Exception("Broken record for {$object['id']}");
+            }
+
+            $attr_acc[$record['name']] = $record['value'];
+            return $attr_acc;
+          },
+          array()
+        );
+
+        $info = array_merge($info, $attrs);
+        return $info;
+      },
+      $rt_objects
+    );
   }
 
   public static function find($params = array()) {
--- init.php
+++ init.php
@@ -22,34 +22,6 @@ use \Racknews\AppController as AppController;
 use \Racknews\ObjectsController as ObjectsController;
 use \Racknews\IPv4Controller as IPv4Controller;
 
-function get_objects() {
-  $rt_objects = scanRealmByText('object');
-
-  return array_map(
-    function ($object) {
-      $info = spotEntity('object', $object['id']);
-      amplifyCell($info);
-
-      $attrs = array_reduce(
-        getAttrValues($object['id']),
-        function ($attr_acc, $record) {
-          if (!isset($record['name'])) {
-            throw new \Exception("Broken record for {$object['id']}");
-          }
-
-          $attr_acc[$record['name']] = $record['value'];
-          return $attr_acc;
-        },
-        array()
-      );
-
-      $info = array_merge($info, $attrs);
-      return $info;
-    },
-    $rt_objects
-  );
-}
-
 if (!function_exists('loadIPv4AddrList')) {
   function loadIPv4AddrList(&$info) {
     \loadIPAddrList($info);
